1f948260e26a934550f84dbf2646491330b27d4d
Sergio Peschiera <speschiera@gmail.com>
2026-02-27 16:54:36 +0000

    Fix production white screen by removing React alias overrides

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        react: path.resolve(__dirname, 'node_modules/react'),
-        'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-        'react-reconciler': path.resolve(__dirname, 'node_modules/react-reconciler'),
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
